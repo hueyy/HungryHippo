5d4767a1ef046615be2dcad80adff53264626256
Huey <leejinhuey@gmail.com>
2019-08-30 10:01:18 +0800

    fixed content-type for home

--- routes/Home.js
+++ routes/Home.js
@@ -1,4 +1,6 @@
-const Home = (req, res) =>
-  res.status(200).send(`See the <a href="https://github.com/hueyy/HungryHippo">Github repository here</a>`)
+const Home = (req, res) => {
+  res.header(`Content-Type`, `text/html`)
+  return res.status(200).send(`See the <a href="https://github.com/hueyy/HungryHippo">Github repository here</a>`)
+}
 
 module.exports = Home
