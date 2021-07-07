e35fe189a38188e7279035a910b24106cb38d991
Huey <hello@huey.xyz>
2021-07-07 19:11:47 +0800

    edge now takes arbitrary URLs

--- src/muncher/individual-sites/theedgesingapore.com/Edge.ts
+++ src/muncher/individual-sites/theedgesingapore.com/Edge.ts
@@ -1,25 +1,27 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+import type { IndividualSiteMuncher } from '../types'
 
 const BASE_URL = `https://www.theedgesingapore.com`
 
-const edgeMuncher = async () => {
-  const { data } = await axios.get(`${BASE_URL}/section/latest`)
+const edgeMuncher: IndividualSiteMuncher = async (url) => {
+  const { data } = await axios.get(url || `${BASE_URL}/section/latest`)
 
   const $ = cheerio.load(data)
 
-  const items = $(`.content .node-article`).map((_, el) => ({
-    title: $(`.news-title`, el).text().trim(),
-    content: $(`.news-desc`, el).text().trim(),
-    link: BASE_URL + $(`.news-title > a`, el).attr(`href`),
-    image: $(`.news-image img`, el).attr(`src`)
+  const items = $(`.content .node-article`).map((_, element) => ({
+    content: $(`.news-desc`, element).text().trim(),
+    date: new Date($(`p.article-post-date`, element).text().trim()),
+    image: $(`.news-image img`, element).attr(`src`),
+    link: BASE_URL + $(`.news-title > a`, element).attr(`href`),
+    title: $(`.news-title`, element).text().trim()
   })).get()
 
   return {
-    title: $(`title`).first().text().trim(),
     description: $(`meta[property='og:description']`).text().trim(),
     items,
     link: BASE_URL,
+    title: $(`title`).first().text().trim(),
   }
 }
 
