0abe334733b2c6d0b5ca7eb9f3f7700fadd6227c
Huey <hello@huey.xyz>
2022-12-18 11:18:28 +0800

    feature: add SAL Practitioner

---
+++ src/muncher/individual-sites/academypublishing.org.sg/SALPractitioner.ts
@@ -0,0 +1,31 @@
+import Request from "../Request"
+import * as cheerio from 'cheerio'
+import type { IndividualSiteMuncher } from "../types"
+
+const BASE_URL = `https://journalsonline.academypublishing.org.sg`
+const site = `${BASE_URL}/Journals/SAL-Practitioner/`
+
+const SALPractitionerMuncher: IndividualSiteMuncher = async (_, { area }) => {
+  const { data } = await Request.get(`${site}${area}`)
+  const $ = cheerio.load(data)
+
+  const items = $(`.ArticleListing tbody > tr`).map((_, element) => ({
+    content: $(element).html(),
+    date: new Date(
+      $(`h4`, element)
+        .text().trim()
+        .replace(`Published on e-First `, ``)
+    ),
+    link: $(`a`, element).attr(`href`),
+    title: $(`.artListItem > p:nth-of-type(1)`, element).text().trim()
+  })).get().filter(item => item.title.length > 0)
+
+  return {
+    description: $(`meta[property='og:description']`).text().trim(),
+    items: items,
+    link: site,
+    title: $(`title`).first().text().trim()
+  }
+}
+
+export default SALPractitionerMuncher
--- src/muncher/individual-sites/academypublishing.org.sg/index.ts
+++ src/muncher/individual-sites/academypublishing.org.sg/index.ts
@@ -1,9 +1,11 @@
 import AnnualReviewOfCases from './annual-review-of-cases'
 import Journal from './sal-journal'
 import SpecialIssue from './special-issue'
+import SALPractitioner from './SALPractitioner'
 
 export default {
   "annual-review-of-cases": AnnualReviewOfCases,
   "sal-journal": Journal,
+  "sal-practitioner": SALPractitioner,
   "special-issue": SpecialIssue,
 }
